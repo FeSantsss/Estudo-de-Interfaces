fbc8fc200357701fa6594fc92803e23d238ac9c8
Felipy <tercetime18@gmail.com>
2025-06-28 20:34:57 -0300

    modified 1 solution: add try-catch with-resource

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,29 +14,32 @@ public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
 		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-	
-		System.out.println("Entre com os dados do aluguel");
-		
-		System.out.print("Modelo do carro: ");
-		String carModel = sc.nextLine();
-		System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
-		LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
-		System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
-		LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
-		
-		CarRental car = new CarRental(start, finish, new Vehicle(carModel));
-		
-		System.out.print("Entre com o preço por hora: ");
-		double pricePerHour = sc.nextDouble();
-		System.out.print("Entre com o preço por dia: ");
-		double pricePerDay = sc.nextDouble();
-		
-		RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
-		rentalService.processInvoice(car);
-		
-		System.out.println(car.toString());
+		try (Scanner sc = new Scanner(System.in);) {
+
+			System.out.println("Entre com os dados do aluguel");
+
+			System.out.print("Modelo do carro: ");
+			String carModel = sc.nextLine();
+			System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
+			LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+			System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
+			LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
+			CarRental car = new CarRental(start, finish, new Vehicle(carModel));
+
+			System.out.print("Entre com o preço por hora: ");
+			double pricePerHour = sc.nextDouble();
+			System.out.print("Entre com o preço por dia: ");
+			double pricePerDay = sc.nextDouble();
+
+			RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+			rentalService.processInvoice(car);
+
+			System.out.println(car.toString());
+		} catch (Exception e) {
+			System.out.println("Erro: " + e.getMessage());
+		}
 	}
-		
+
 }
